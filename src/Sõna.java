f20414f50e16bfcbc4996990177d6c048cd5e854
Heidi <heidi.meier@gmail.com>
2017-03-31 22:58:06 +0300

    Tegin kaks faili: Sõna klassi ja Tekstianalyysi klassi. Sõna objekte ka sorteeritakse.

---
+++ src/Sõna.java
@@ -0,0 +1,38 @@
+
+
+/**
+ * Created by heids on 31.03.2017.
+ */
+public class Sõna implements Comparable <Sõna>{
+    String nimetus;
+    int pikkus;
+
+    @Override
+    public int compareTo(Sõna võrreldav) {
+        if (pikkus < võrreldav.pikkus){
+            return 1;
+        }
+        if (pikkus > võrreldav.pikkus){
+            return -1;
+        }
+        return 0;
+    }
+
+    public Sõna(String nimetus, int pikkus) {
+        this.nimetus = nimetus;
+        this.pikkus = pikkus;
+    }
+
+    public int getPikkus() {
+        return pikkus;
+    }
+
+    public String getNimetus() {
+        return nimetus;
+    }
+
+    @Override
+    public String toString() {
+        return nimetus + " " + pikkus + "\n";
+    }
+}
---
+++ src/TekstiAnalyys.java
@@ -0,0 +1,38 @@
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ * Created by heids on 31.03.2017.
+ */
+public class TekstiAnalyys {
+    public static void main(String[] args) throws FileNotFoundException {
+        File fail = new File("president.txt");
+        Scanner sc = new Scanner(fail, "UTF-8");
+        ArrayList<Sõna> list1 = new ArrayList<>();
+        String[] tükid = new String[0];
+        double kokku = 0;
+        while (sc.hasNextLine()) {
+            String rida = sc.nextLine();
+            tükid = rida.split(" ");
+            for (int i = 0; i < tükid.length; i++ ){
+                String nimi = tükid[i].replaceAll(",", "").replaceAll("!", "").replaceAll("\\.", "").replaceAll("\\?", "").replaceAll(" ", "").replaceAll("–", "");
+                String strNimi = nimi.toLowerCase();
+                int intNumber = tükid[i].length();
+                kokku = kokku + tükid[i].length();
+                Sõna a = new Sõna(strNimi, intNumber);
+                list1.add(a);
+            }
+        }
+        double keskminePikkus = kokku / list1.size();
+        Collections.sort(list1);
+        String strList1 = list1.toString().replaceAll("[\\[\\]]", "").replaceAll(",", "");
+        System.out.println("Keskmine sõnepikkus: " + String.format("%.2f", keskminePikkus));
+        System.out.println("Sõned pikkuse järjekorras: ");
+        System.out.println(strList1);
+    }
+}
